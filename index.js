7239babecaa449abc92e671690087ee32a1d42c5
carecoding <118536562+carecoding@users.noreply.github.com>
2022-11-22 00:07:07 +0800

    加入跨域的response头部。

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
 app.use(express.static('public'))
+
+//***以下设置用于跨域，否则无法访问***/
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header('X-Powered-By', ' 3.2.1')
+    req.method == "OPTIONS" ? res.send(200) : next()
+  });
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
